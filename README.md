855e3a42d76a3bd69706323387166cef34e7e491
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2024-07-10 14:32:52 +0200

    Add flake.nix

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cardano-dev
 Scripts for developing on cardano
+
+## Nix usage
+Theres an app in the flake for every script in `scripts/` directory.
+You can view them using `nix flake show github:input-output-hk/cardano-dev`.
+The scripts can be run using `nix run` e.g.:
+```bash
+# execute scripts/tag.sh script
+nix run 'github:input-output-hk/cardano-dev#tag'
+```
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A list of useful scripts for cardano-node development";
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.systems.follows = "systems";
+  };
+
+  outputs = { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # list of all scripts (without .sh suffix)
+        scripts = with builtins;
+          map (pkgs.lib.removeSuffix ".sh") (attrNames (readDir ./scripts));
+        # create an app for a script name
+        mkScriptApp = scriptName: {
+          type = "app";
+          program = pkgs.lib.getExe ((pkgs.writeScriptBin scriptName
+            (builtins.readFile ./scripts/${scriptName}.sh)).overrideAttrs
+            (old: {
+              buildCommand = ''
+                ${old.buildCommand}
+                 patchShebangs $out'';
+            }));
+        };
+      in {
+        # create an app for every script in `scripts` directory
+        apps = pkgs.lib.foldl' (acc: s: acc // { ${s} = mkScriptApp s; }) { }
+          scripts;
+
+        formatter = nixpkgs.legacyPackages.${system}.nixfmt;
+      });
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = true;
+  };
+}
