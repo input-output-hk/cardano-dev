989862593fea7f23adb3a87b3c789b471e4ece98
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2026-08-24 17:29:47 +0200

    Remove x86_64-darwin from supported architectures

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       utils,
       ...
     }@inputs:
-    utils.lib.eachDefaultSystem (
+    utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ] (
       system:
       let
         pkgs = import nixpkgs {
